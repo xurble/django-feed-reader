7543e21dc27dd1a8e4bfbc4b7a1a3ec037fc815b
Chris Spencer <cspencer@edgeondemand.com>
2019-09-01 00:22:17 -0400

    Fixed date string bug breaking source add. Fleshed out model admins for main models.

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,9 +1,48 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 from feeds import models
 
-admin.site.register(models.Source)
-admin.site.register(models.Post)
-admin.site.register(models.Enclosure)
+class SourceAdmin(admin.ModelAdmin):
+
+    readonly_fields = (
+        'posts_link',
+    )
+
+    def posts_link(self, obj=None):
+        if not obj:
+            return ''
+        qs = obj.posts.all()
+        return mark_safe('<a href="/admin/feeds/post/?source__id=%i" target="_blank">%i Posts</a>' % (obj.id, qs.count()))
+    posts_link.short_description = 'posts'
+
+class PostAdmin(admin.ModelAdmin):
+
+    raw_id_fields = ('source',)
+
+    list_display = ('title', 'source', 'created', 'guid', 'author')
+
+    search_fields = ('title',)
+
+    readonly_fields = (
+        'enclosures_link',
+    )
+
+    def enclosures_link(self, obj=None):
+        if not obj:
+            return ''
+        qs = obj.enclosures.all()
+        return mark_safe('<a href="/admin/feeds/enclosure/?post__id=%i" target="_blank">%i Enclosures</a>' % (obj.id, qs.count()))
+    enclosures_link.short_description = 'enclosures'
+
+class EnclosureAdmin(admin.ModelAdmin):
+
+    raw_id_fields = ('post',)
+
+    list_display = ('href', 'type')
+
+admin.site.register(models.Source, SourceAdmin)
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Enclosure, EnclosureAdmin)
 admin.site.register(models.WebProxy)
